27d47720e08ff0b1e831c74e2a0b3a126cca73f3
chachora <chachora@gmail.com>
2019-10-26 08:32:05 +0400

    Add credit cards end points

---
+++ controller/credit-cards.js
@@ -0,0 +1,12 @@
+const getAll = (req, res) => {
+  res.send('respond with a resource');
+};
+
+const add = (req, res) => {
+  res.send('respond with a resource');
+};
+
+module.exports = Object.freeze({
+  getAll,
+  add,
+});
---
+++ routes/credit-cards.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const controller = require('../controller/credit-cards');
+
+const router = express.Router();
+
+/**
+ * GET credit cards
+ */
+router.get('/', controller.getAll);
+
+/**
+ * Add credit card
+ */
+router.post('/', controller.add);
+
+module.exports = router;
