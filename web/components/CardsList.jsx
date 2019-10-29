be7d50ab2747621544b1403e1782ecd88cda401b
Yuriy Chachora <chachora@gmail.com>
2019-10-29 14:32:42 -0700

    Add initial CardsList implementation

--- web/components/CardsList.jsx
+++ web/components/CardsList.jsx
@@ -4,6 +4,24 @@ const CardsList = () => {
   return (
     <div>
       <h4>Existing Cards</h4>
+      <table border={1} cellPadding={8}>
+        <thead>
+          <tr bgcolor="#d3d3d3">
+            <th>Name</th>
+            <th>Card Number</th>
+            <th>Balance</th>
+            <th>Limit</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Alice</td>
+            <td>1111 2222 3333 4444</td>
+            <td>£10.24</td>
+            <td>£5000</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
