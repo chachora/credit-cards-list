3118514b5f68ebd377c197d43a517319995be7e5
Yuriy Chachora <chachora@gmail.com>
2019-10-29 14:15:28 -0700

    Add fields to AddCardForm

--- web/components/AddCardForm.jsx
+++ web/components/AddCardForm.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import InputField from './InputField';
 
 const AddCardForm = () => {
   return (
     <div>
       <h4>Add</h4>
+      <form>
+        <InputField label="Name" id="name" />
+        <InputField label="Card number" id="card_number" />
+        <InputField label="Limit" id="limit" />
+      </form>
+      <button type="button">Add</button>
     </div>
   );
 };
