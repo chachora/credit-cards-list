cd8148dc8cc268eecb8b27682341208c1e9b4a54
Yuriy Chachora <chachora@gmail.com>
2019-10-29 22:20:42 -0700

    Update InputField to handle value change

--- web/components/InputField.jsx
+++ web/components/InputField.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './InputField.css';
 
-const InputField = ({ id, label, type }) => {
+const InputField = ({ id, label, type, value, onChange }) => {
   return (
     <div className={styles.container}>
       <label htmlFor={id}>{label}</label>
-      <input type={type} id={id} />
+      <input type={type} id={id} value={value} onChange={onChange} />
     </div>
   );
 };
@@ -19,6 +19,8 @@ InputField.propTypes = {
   id: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   type: PropTypes.string,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
 export default InputField;
