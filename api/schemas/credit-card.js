9bc3a88c8ce11e1f50260ae20055391c85159f2c
Yuriy Chachora <chachora@gmail.com>
2019-11-02 14:10:27 -0700

    Add credit card number validation

--- api/schemas/credit-card.js
+++ api/schemas/credit-card.js
@@ -5,16 +5,8 @@ const schema = Joi.object({
     .min(3)
     .required(),
   number: Joi.string()
-    .pattern(/^[0-9]{13,19}$/)
-    .required()
-    .error(errors => {
-      errors.forEach(err => {
-        if (err.code === 'string.pattern.base') {
-          err.message = 'Wrong credit card number';
-        }
-      });
-      return errors;
-    }),
+    .creditCard()
+    .required(),
   limit: Joi.number().required(),
 });
 
